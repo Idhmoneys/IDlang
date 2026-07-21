77f3ae2afcc5e27e42279a7b22fb40b4a8e9f144
Idhm <idhmoneys@gmail.com>
2026-07-21 20:13:09 +0700

    making the parser and the interpreter, current status: can do basic arthmetic

--- README.md
+++ README.md
@@ -1,16 +1,116 @@
+![Python](https://img.shields.io/badge/Python-3.11-blue)
+
+![License](https://img.shields.io/badge/license-MIT-green)
+
+![Status](https://img.shields.io/badge/status-In%20Development-orange)
+
+# 🇮🇩 Indo Programming Language
+
+🇬🇧 English below.
+
+Bahasa pemrograman sederhana yang memakai keyword bahasa Indonesia.
+Project ini dibuat sebagai sarana belajar membuat interpreter dari nol.
+
+---
+
+A simple interpreted programming language using Indonesian keywords.
+This project is built to learn how programming languages work.
+
+> 🚧 This project is under active development.
+> Syntax and features may change in future versions.
+
+## 📝 Example
+
+```bash
+$ python main.py
+
+id: 25 + 6 * 15 : 2 - 3
+67
+id: 20 : (-5 - (-10))
+4.0
+
+```
+
+## 📸 Screenshot
+
+v0.1
+<p align="center">
+  <img src="assets/interpreter_v0.1.jpg" alt="Something went wrong..." />
+</p>
+
+## 📋 Requirements
+
+- Python 3.11+
+
+## ⚙️ Installation
+
+``` bash
+git clone https://github.com/Idhmoneys/IDlang.git
+cd IDlang
+python main.py
+```
+
+## ✨ Features
+
+- Interactive REPL
+- Lexer
+- Parser
+- Abstract Syntax Tree (AST)
+- Interpreter
+- Arithmetic operations
+- Unary operators
+- Parentheses support
+
+## 💈 Current Status
+
+The language is currently capable of evaluating arithmetic expressions.
+
+✅ Supported features:
+
+- Integer
+- Float
+- '+ - * :'
+- Unary operators
+- Parentheses
+
+## 🛣️ Roadmap
+
+- [x] Lexer
+- [x] Parser
+- [x] AST
+- [x] Interpreter
+- [ ] Variables
+- [ ] String
+- [ ] Boolean
+- [ ] If statement
+- [ ] Loop
+- [ ] Function
+- [ ] Lists
+- [ ] Dictionaries
+- [ ] Module
+- [ ] Standard Library
+- [ ] Error Traceback
+
+## 🤷‍♂️ Why?
+
+This project was created to learn how programming languages and interpreters work while experimenting with an Indonesian-style syntax.
+
+The long-term goal is to make programming feel more natural for Indonesian speakers.
+
 ## ===== PROJECT INFO =====
 
-**Project Name:**\
-idlang
+*Project Name:*\
+IDlang
 
-**Made By:**\
-Idhm
+*Made By:*\
+[Idhm](https://github.com/Idhmoneys)
 
-**Made With:**\
+*Made With:*\
 Python
 
-**Date:**\
-19/07/2026
+*Made in:*\
+19/07/2026 (20:03:52)
 
-**Time:**\
-20:03:52
+## License
+
+MIT
--- id_.py
+++ id_.py
@@ -5,7 +5,7 @@
 DIGITS = '0123456789'
 
 ############################
-# TOKENS
+# ERROR 
 ############################
 
 class Error:
@@ -20,23 +20,34 @@ class Error:
     result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
     return result
     
-class IleggalCharError(Error):
+class IllegalCharError(Error):
   def __init__(self, pos_start, pos_end, details):
-    super().__init__(pos_start, pos_end, 'Ileggal Character', details)
+    super().__init__(pos_start, pos_end, 'Illegal Character', details)
+    
+class InvalidSyntaxError(Error):
+  def __init__(self, pos_start, pos_end, details=''):
+    super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
 ############################
-# TOKENS
+# POSITION 
 ############################
 
 class Position:
   def __init__(self, idx, ln, col, fn, ftxt):
+    """
+    idx = index
+    ln = line number
+    col = column
+    fn = file name
+    ftxt = file text
+    """
     self.idx = idx
     self.ln = ln
     self.col = col
     self.fn = fn
     self.ftxt = ftxt
     
-  def advance(self, current_char):
+  def advance(self, current_char=None):
     self.idx += 1
     self.col += 1
     
@@ -50,10 +61,10 @@ class Position:
     return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 ############################
-# TOKENS
+# TOKENS 
 ############################
 
-# Tambah TT (toket type)
+# Tambah TT (token type)
 TT_INT        = 'INT'
 TT_FLOAT      = 'FLOAT'
 TT_PLUS       = 'PLUS'
@@ -62,13 +73,22 @@ TT_MUL        = 'MUL'
 TT_DIV        = 'DIV'
 TT_LPARENT    = 'LPARENT'
 TT_RPARENT    = 'RPARENT'
+TT_EOF        = 'EOF'
 
 # BUAT CLASS NYA
 
 class Token:
-  def __init__(self, type_, value=None):
+  def __init__(self, type_, value=None, pos_start=None, pos_end=None):
     self.type = type_
     self.value = value
+    
+    if pos_start:
+      self.pos_start = pos_start
+      self.pos_end = pos_start.copy()
+      self.pos_end.advance()
+      
+    if pos_end:
+      self.pos_end = pos_end
     
   def __repr__(self):
     if self.value is not None: return f'{self.type}:{self.value}'
@@ -76,7 +96,7 @@ class Token:
     
     
 ############################
-# LEXER
+# LEXER 
 ############################
 
 class Lexer:
@@ -94,42 +114,44 @@ class Lexer:
   def make_token(self):
     tokens = []
     
-    while self.current_char != None:
+    while self.current_char is not None:
       if self.current_char in ' \t':
         self.advance()
       elif self.current_char in DIGITS:
         tokens.append(self.make_number())
       elif self.current_char == '+':
-        tokens.append(Token(TT_PLUS))
+        tokens.append(Token(TT_PLUS, pos_start=self.pos))
         self.advance()
       elif self.current_char == '-':
-        tokens.append(Token(TT_MINUS))
+        tokens.append(Token(TT_MINUS, pos_start=self.pos))
         self.advance()
       elif self.current_char == '*':
-        tokens.append(Token(TT_MUL))
+        tokens.append(Token(TT_MUL, pos_start=self.pos))
         self.advance()
-      elif self.current_char == '/':
-        tokens.append(Token(TT_DIV))
+      elif self.current_char == ':':
+        tokens.append(Token(TT_DIV, pos_start=self.pos))
         self.advance()
       elif self.current_char == '(':
-        tokens.append(Token(TT_LPARENT))
+        tokens.append(Token(TT_LPARENT, pos_start=self.pos))
         self.advance()
       elif self.current_char == ')':
-        tokens.append(Token(TT_RPARENT))
+        tokens.append(Token(TT_RPARENT, pos_start=self.pos))
         self.advance()
       else:
         pos_start = self.pos.copy()
         char = self.current_char
         self.advance()
-        return [], IleggalCharError(pos_start, self.pos, "'" + char + "'")
+        return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
         
+    tokens.append(Token(TT_EOF, pos_start=self.pos))
     return tokens, None
     
   def make_number(self):
     num_str = ''
     dot_count = 0
+    pos_start = self.pos.copy()
     
-    while self.current_char != None and self.current_char in DIGITS + '.':
+    while self.current_char is not None and self.current_char in DIGITS + '.':
       if self.current_char == '.':
         if dot_count == 1: break
         dot_count += 1
@@ -139,16 +161,237 @@ class Lexer:
       self.advance()
         
     if dot_count == 0:
-      return Token(TT_INT, int(num_str))
+      return Token(TT_INT, int(num_str), pos_start, self.pos)
     else:
-      return Token(TT_FLOAT, float(num_str))
+      return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
       
 ############################
-# RUN
+# NODES 
+############################
+
+class NumberNode:
+  def __init__(self, token):
+    self.token = token
+    
+    self.pos_start = self.token.pos_start
+    self.pos_end = self.token.pos_end
+    
+  def __repr__(self):
+    return f'{self.token}'
+    
+class BinaryOpNode: # Op -> operation
+  def __init__(self, left_node, op_token, right_node):
+    self.left_node = left_node
+    self.op_token = op_token
+    self.right_node = right_node
+    
+    self.pos_start = self.left_node.pos_start
+    self.pos_end = self.right_node.pos_end
+    
+  def __repr__(self):
+    return f'({self.left_node}, {self.op_token}, {self.right_node})'
+    
+class UnaryOpNode:
+  def __init__(self, op_token, node):
+    self.op_token = op_token
+    self.node = node
+    
+    self.pos_start = self.op_token.pos_start
+    self.pos_end = self.op_token.pos_end
+    
+  def __repr__(self):
+    return f'({self.op_token}, {self.node})'
+
+############################
+# PARSE RESULT 
+############################
+
+class ParseResult:
+  def __init__(self):
+    self.error = None
+    self.node = None
+    
+  def register(self, res):
+    if isinstance(res, ParseResult):
+      if res.error: self.error = res.error
+      return res.node
+      
+    return res
+  
+  def success(self, node):
+    self.node = node
+    return self
+    
+  def failure(self, error):
+    self.error = error
+    return self
+
+############################
+# PARSER 
+############################
+
+class Parser:
+  def __init__(self, tokens):
+    self.tokens = tokens
+    self.token_idx = -1
+    self.advance()
+    
+  def advance(self):
+    self.token_idx += 1
+    if self.token_idx < len(self.tokens):
+      self.current_token = self.tokens[self.token_idx]
+    return self.current_token
+    
+  def parse(self):
+    res = self.expr()
+    if not res.error and self.current_token.type != TT_EOF:
+      return res.failure(InvalidSyntaxError(
+        self.current_token.pos_start,
+        self.current_token.pos_end,
+        "Expected '+', '-', '*', or ':'"
+      ))
+    return res
+    
+  def factor(self):
+    res = ParseResult()
+    token = self.current_token
+    
+    if token.type in (TT_PLUS, TT_MINUS):
+      res.register(self.advance())
+      factor = res.register(self.factor())
+      if res.error: return res
+      return res.success(UnaryOpNode(token, factor))
+    
+    elif token.type in (TT_INT, TT_FLOAT):
+      res.register(self.advance())
+      return res.success(NumberNode(token))
+      
+    elif token.type == TT_LPARENT:
+      res.register(self.advance())
+      expr = res.register(self.expr())
+      if res.error: return res
+      if self.current_token.type == TT_RPARENT:
+        res.register(self.advance())
+        return res.success(expr)
+      else:
+        return res.failure(InvalidSyntaxError(
+          self.current_token.pos_start, self.current_token.pos_end, "Expected ')'"
+        ))
+      
+    return res.failure(InvalidSyntaxError(
+      token.pos_start, token.pos_end, "Expected int or float"
+    ))
+    
+  def term(self):
+    return self.binary_op(self.factor, (TT_MUL, TT_DIV))
+    
+  def expr(self):
+    return self.binary_op(self.term, (TT_PLUS, TT_MINUS))
+    
+  def binary_op(self, func, ops):
+    res = ParseResult()
+    left = res.register(func())
+    if res.error: return res 
+    
+    while self.current_token.type in ops:
+      op_token = self.current_token
+      res.register(self.advance())
+      right = res.register(func())
+      if res.error: return res 
+      left = BinaryOpNode(left, op_token, right)
+    return res.success(left)
+
+############################
+# VALUES 
+############################
+
+class Number:
+  def __init__(self, value):
+    self.value = value
+    self.set_pos()
+    
+  def set_pos(self, pos_start=None, pos_end=None):
+    self.pos_start = pos_start
+    self.pos_end   = pos_end
+    return self
+    
+  def added_to(self, other):
+    if isinstance(other, Number):
+      return Number(self.value + other.value)
+      
+  def subbed_by(self, other):
+    if isinstance(other, Number):
+      return Number(self.value - other.value)
+      
+  def multiply_by(self, other):
+    if isinstance(other, Number):
+      return Number(self.value * other.value)
+      
+  def divided_by(self, other):
+    if isinstance(other, Number):
+      return Number(self.value / other.value)
+      
+  def __repr__(self):
+    return str(self.value)
+
+############################
+# INTERPRETER 
+############################
+
+class Interpreter:
+  def visit(self, node):
+    method_name = f'visit_{type(node).__name__}'
+    method = getattr(self, method_name, self.no_visit_method)
+    return method(node)
+    
+  def no_visit_method(self, node):
+    raise Exception(f'No visit {type(node).__name__} method defined')
+    
+  ###########################
+  
+  def visit_NumberNode(self, node):
+    return Number(node.token.value).set_pos(node.pos_start, node.pos_end)
+    
+  def visit_BinaryOpNode(self, node):
+    left  = self.visit(node.left_node)
+    right = self.visit(node.right_node)
+    
+    if node.op_token.type == TT_PLUS:
+      result = left.added_to(right)
+    elif node.op_token.type == TT_MINUS:
+      result = left.subbed_by(right)
+    elif node.op_token.type == TT_MUL:
+      result = left.multiply_by(right)
+    elif node.op_token.type == TT_DIV:
+      result = left.divided_by(right)
+      
+    return result.set_pos(node.pos_start, node.pos_end)
+    
+  def visit_UnaryOpNode(self, node):
+    number = self.visit(node.node)
+    
+    if node.op_token.type == TT_MINUS:
+      number = number.multiply_by(Number(-1))
+      
+    return number.set_pos(node.pos_start, node.pos_end)
+
+############################
+# RUN 
 ############################
 
 def run(fn, text):
-  lexer = Lexer(fn, text)
+  # GENERATE TOKENS
+  lexer: Lexer = Lexer(fn, text)
   tokens, error = lexer.make_token()
+  if error: return None, error
   
-  return tokens, error
+  # GENERATE AST
+  parser: Parser = Parser(tokens)
+  ast = parser.parse()
+  if ast.error: return None, ast.error
+  
+  # RUN PROGRAM
+  interpreter: Interpreter = Interpreter()
+  result = interpreter.visit(ast.node)
+  
+  return result, None
